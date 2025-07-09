98a767b4108fc94109ba31b7a1cc16ddb38cbe0e
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-07-09 18:57:30 +0300

    Create Query.cs

---
+++ InsightNotes.Api/InsightNotes.Api/GraphQL/Query.cs
@@ -0,0 +1,11 @@
+﻿using InsightNotes.Api.Models;
+using InsightNotes.Api.Services;
+
+namespace InsightNotes.Api.GraphQL
+{
+    public class Query
+    {
+        public IEnumerable<Note> GetNotes([Service] NoteService noteService) =>
+            noteService.GetNotes();
+    }
+}
