af272516452475639aeea90b162c90b452274471
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-07-09 18:52:03 +0300

    Create Note service basic definition

---
+++ InsightNotes.Api/InsightNotes.Api/Services/NoteService.cs
@@ -0,0 +1,20 @@
+﻿using InsightNotes.Api.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace InsightNotes.Api.Services
+{
+    public class NoteService
+    {
+        private readonly List<Note> notes = new List<Note>();
+
+        public IEnumerable<Note> GetNotes() => notes;
+
+        public Note AddNote(string title, string content)
+        {
+            Note note = new Note(title, content);
+            notes.Add(note);
+
+            return note;
+        }
+    }
+}
