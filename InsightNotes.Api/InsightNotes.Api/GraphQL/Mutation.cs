fc76af8b763fe31c7ed36c452b33775ec03073cb
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-07-15 15:57:34 +0300

    Refactor Mutation to use constructor injection and add update/delete
    Refactored the Mutation class to use constructor injection for NoteService instead of method injection. Added UpdateNoteAsync and DeleteNoteAsync methods to support updating and deleting notes via GraphQL mutations.

--- InsightNotes.Api/InsightNotes.Api/GraphQL/Mutation.cs
+++ InsightNotes.Api/InsightNotes.Api/GraphQL/Mutation.cs
@@ -1,12 +1,27 @@
 ﻿using InsightNotes.Api.Models;
-
+using InsightNotes.Api.Services;
 
 namespace InsightNotes.Api.GraphQL
 {
     public class Mutation
     {
-        public async Task<Note> AddNoteAsync(string title, string content, [Service] NoteService noteService) =>
+        private readonly NoteService noteService;
+
+        public Mutation(NoteService noteService)
+        {
+            this.noteService = noteService;
+        }
+
+        public async Task<Note> AddNoteAsync(string title, string content) =>
             await noteService.AddNoteAsync(title, content);
+
+        public async Task<Note> UpdateNoteAsync(Guid noteId, string newTitle, string newContent) =>
+            await noteService.UpdateNoteAsync(noteId, newTitle, newContent);
+
+        public async Task<bool> DeleteNoteAsync(Guid noteId)
+        {
+            await noteService.DeleteNoteAsync(noteId);
+            return true; // or some status if you want to handle failure
+        }
     }
 }
-
