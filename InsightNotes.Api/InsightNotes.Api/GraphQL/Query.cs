264b1391d571e24ce09cf5eff3d23168a9a72909
MartiHr <81310884+MartiHr@users.noreply.github.com>
2025-07-15 15:57:25 +0300

    Add new note query methods to GraphQL Query class
    Introduced methods to fetch a note by ID, fetch multiple notes by IDs, and search notes with a query and limit. These additions enhance the GraphQL API's ability to retrieve notes in more flexible ways.

--- InsightNotes.Api/InsightNotes.Api/GraphQL/Query.cs
+++ InsightNotes.Api/InsightNotes.Api/GraphQL/Query.cs
@@ -1,4 +1,5 @@
 ﻿using InsightNotes.Api.Models;
+using InsightNotes.Api.Services;
 
 namespace InsightNotes.Api.GraphQL
 {
@@ -11,9 +12,16 @@ namespace InsightNotes.Api.GraphQL
             this.noteService = noteService;
         }
 
-        public async Task<IEnumerable<Note>> GetNotesAsync()
-        {
-            return await noteService.GetNotesAsync();
-        }
+        public async Task<IEnumerable<Note>> GetNotesAsync() =>
+            await noteService.GetNotesAsync();
+
+        public async Task<Note?> GetNoteByIdAsync(Guid noteId) =>
+            await noteService.GetNoteByIdAsync(noteId);
+
+        public async Task<IEnumerable<Note>> GetNotesByIdsAsync(IEnumerable<Guid> noteIds) =>
+            await noteService.GetNotesByIdsAsync(noteIds);
+
+        public async Task<IEnumerable<Note>> SearchNotesAsync(string query, int limit = 5) =>
+            await noteService.SearchNotesAsync(query, limit);
     }
 }
